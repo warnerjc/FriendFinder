86e822582c7a0d81602925ded84c101964ced7bc
Jason Warner <jcwarner@gmail.com>
2018-05-01 22:47:08 -0500

    Initial routing of Friend Finder application

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 // Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function () {
+    console.log('Friend Finder is listening on PORT: ' + PORT);
+});
